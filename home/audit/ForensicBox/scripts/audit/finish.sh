02adb9cea74d25807ecc5da90582fbba33fca964
João Paixão <36067302+Joao-Paixao@users.noreply.github.com>
2023-07-20 17:40:01 +0100

    Update finish.sh

--- home/audit/ForensicBox/scripts/audit/finish.sh
+++ home/audit/ForensicBox/scripts/audit/finish.sh
@@ -7,11 +7,17 @@ mv /home/audit/ForensicBox/outputs/report*.pdf /var/www/forensicbox/userspace/$E
 # Remove audit outputs
 sudo rm /home/audit/ForensicBox/outputs/*
 
+# Stop wpa_supplicant
+sudo kill $(sudo pgrep wpa_supplicant)
+# Release DHCP lease
+sudo dhclient -r wlan0
+
 # Remove network connections
 sudo systemctl stop wpa_supplicant
 sudo rm /var/run/wpa_supplicant/wlan0
-cp /home/audit/ForensicBox/defaults/dhcpcd_deactivate_wlan0.conf /etc/dhcpcd.conf
-sudo systemctl restart dhcpcd
+sudo cp /home/audit/ForensicBox/defaults/dhcpcd_deactivate_wlan0.conf /etc/dhcpcd.conf
+sudo cp /home/audit/ForensicBox/defaults/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
+sudo systemctl restart dhcpcd.service
 
 sudo ifconfig eth0 down
 
