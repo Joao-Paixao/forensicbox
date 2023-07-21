ef8bb2a233788dd63e502cb656200a0b7ebfdcb3
João Paixão <36067302+Joao-Paixao@users.noreply.github.com>
2023-07-21 12:29:18 +0100

    Update init.sh

--- home/audit/ForensicBox/scripts/audit/init.sh
+++ home/audit/ForensicBox/scripts/audit/init.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
 # Turn Off Access Point
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Script: $0 - Turn Off Access Point" >> "/home/audit/ForensicBox/log.txt"
 sudo /home/audit/ForensicBox/scripts/access_point/stop_ap.sh
 
 # Connect to the target Network if not already
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Script: $0 - Connect to Target Network" >> "/home/audit/ForensicBox/log.txt"
 sudo /home/audit/ForensicBox/scripts/audit/connect_network.sh
 
-# Update System Time
-sudo ntpdate pool.ntp.org
+if [ $? -eq 0 ]; then
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - Script: $0 - Successfully Connected" >> "/home/audit/ForensicBox/log.txt"
+        # Update System Time
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - Script: $0 - Updating System Time" >> "/home/audit/ForensicBox/log.txt"
+        sudo ntpdate pool.ntp.org
 
-# Create cron job
-sudo /home/audit/ForensicBox/scripts/audit/schedule.sh
+        # Schedule Audit
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - Script: $0 - Scheduling Audit" >> "/home/audit/ForensicBox/log.txt"
+        sudo /home/audit/ForensicBox/scripts/audit/schedule.sh
+else
+        # Restart
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - Script: $0 - Error Connecting to Target Network (Restarting)" >> "/home/audit/ForensicBox/log.txt"
+        sudo /home/audit/ForensicBox/scripts/audit/finish.sh
+fi
