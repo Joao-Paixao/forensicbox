672e2b1e5715fa15429dcdded3e9d123ccc55e75
João Paixão <36067302+Joao-Paixao@users.noreply.github.com>
2023-07-21 12:30:00 +0100

    Update finish.sh

--- home/audit/ForensicBox/scripts/audit/finish.sh
+++ home/audit/ForensicBox/scripts/audit/finish.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 # Copy report to entity folder
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Script: $0 - Copying Report" >> "/home/audit/ForensicBox/log.txt"
 ENTITY=$(awk '/-connection/ {print $2}' /home/audit/ForensicBox/outputs/audit.conf)
 mv /home/audit/ForensicBox/outputs/report*.pdf /var/www/forensicbox/userspace/$ENTITY/
 
 # Remove audit outputs
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Script: $0 - Removing Outputs" >> "/home/audit/ForensicBox/log.txt"
 sudo rm /home/audit/ForensicBox/outputs/*
+sudo rm /home/audit/ForensicBox/scripts/audit/zap_report/*
+sudo rm /home/audit/ForensicBox/scripts/audit/report/*
 
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Script: $0 - Turn Off Wifi Mode" >> "/home/audit/ForensicBox/log.txt"
 # Stop wpa_supplicant
 sudo kill $(sudo pgrep wpa_supplicant)
 # Release DHCP lease
@@ -19,8 +24,10 @@ sudo cp /home/audit/ForensicBox/defaults/dhcpcd_deactivate_wlan0.conf /etc/dhcpc
 sudo cp /home/audit/ForensicBox/defaults/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
 sudo systemctl restart dhcpcd.service
 
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Script: $0 - Turn Off Ethernet Mode" >> "/home/audit/ForensicBox/log.txt"
 sudo ifconfig eth0 down
 
 # Start access point
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Script: $0 - Restart System" >> "/home/audit/ForensicBox/log.txt"
 sudo /home/audit/ForensicBox/scripts/access_point/stop_ap.sh
 sudo /home/audit/ForensicBox/scripts/access_point/start_ap.sh
