842885f9c6c4b525765b3ee2c411f049b0c329b9
João Paixão <36067302+Joao-Paixao@users.noreply.github.com>
2023-07-21 12:32:36 +0100

    Update start_audit.sh

--- home/audit/ForensicBox/scripts/audit/start_audit.sh
+++ home/audit/ForensicBox/scripts/audit/start_audit.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Script: $0 - Start Audit" >> "/home/audit/ForensicBox/log.txt"
 
 CIDR=$(ip -oneline -family inet addr | awk '/scope global/ {print $4}')
 
-echo $CIDR
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Script: $0 - Initiate Nmap Scan" >> "/home/audit/ForensicBox/log.txt"
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Script: $0 - CIDR: $CIDR" >> "/home/audit/ForensicBox/log.txt"
+sudo nmap -T5 -oX /home/audit/ForensicBox/outputs/nmap.xml -O -sV $CIDR &
 
-echo 'Initiate Nmap Scan'
-sudo nmap -T5 -oX /home/audit/ForensicBox/outputs/nmap.xml -O -sV $CIDR
+nmap_pid=$!
+
+counter=0
+
+while ps -p "$nmap_pid" > /dev/null; do
+        ((counter++))
+        if ((counter == 60)); then
+                echo "$(date '+%Y-%m-%d %H:%M:%S') - Script: $0 - Nmap Scanning" >> "/home/audit/ForensicBox/log.txt"
+                counter=0
+        fi
+        sleep 1
+done
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Script: $0 - Nmap Finish" >> "/home/audit/ForensicBox/log.txt"
+wait "$nmap_pid"
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Script: $0 - Parsing Output" >> "/home/audit/ForensicBox/log.txt"
+sudo python3 /home/audit/ForensicBox/scripts/audit/controller.py /home/audit/ForensicBox/outputs/nmap.xml
